e07e90e4549cbc474f544a190058e20ec6b3e45a
Lil Mosee <c75a90@gmail.com>
2021-03-06 02:45:29 +0800

    Add other declaration ways

--- js/types.js
+++ js/types.js
@@ -1,4 +1,34 @@
-var user = {
+var lilPump = {
     name: 'Lil Pump',
     id: 0
 };
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return UserAccount;
+}());
+var lilYutchy = new UserAccount('Lil Yutchy', 1);
+// function return type declaration
+function getAdminUser() {
+    var admin = {
+        name: 'Admin',
+        id: 999
+    };
+    return admin;
+}
+function deleteUser(user) {
+    console.log(user.name);
+}
+deleteUser(lilYutchy);
+var checkbox = false;
+function getLength(obj) {
+    return obj.length;
+}
+var gg = {
+    add: function (obj) { console.log('add:', obj); },
+    get: function () { return 'abc'; }
+};
+gg.add('sss');
+gg.get();
--- ts/types.ts
+++ ts/types.ts
@@ -1,10 +1,67 @@
 // interface
 interface User {
-  name: String;
-  id: Number;
+  name: string;
+  id: number;
 }
 
-const user: User = {
+const lilPump: User = {
   name: 'Lil Pump',
   id: 0
 }
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const lilYutchy: User = new UserAccount('Lil Yutchy', 1);
+
+// function return type declaration
+function getAdminUser (): User {
+  const admin: User = {
+    name: 'Admin',
+    id: 999
+  };
+
+  return admin;
+}
+
+function deleteUser (user: User) {
+  console.log(user.name);
+}
+deleteUser(lilYutchy);
+
+// Composing Types
+type SwitchStates = true | false;
+type LockStates = 'locked' | 'unlocked';
+
+let checkbox: SwitchStates = false;
+
+function getLength(obj: string | string[]) {
+  return obj.length;
+}
+
+// Generics
+
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+interface Backpack<Type> {
+  add: (obj: Type) => void;
+  get: () => Type;
+}
+
+declare const backpack: Backpack<string>;
+
+const gg: Backpack<string> = {
+  add: (obj) => { console.log('add:', obj) },
+  get: () => { return 'abc' }
+}
+gg.add('sss');
+gg.get();
